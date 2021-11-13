15c51d86fc2fbeaf59fca242667f4a1287aba3a6
evan_daniel_sckans <sckans@Evans-Mac-mini.local>
2021-11-13 00:26:46 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="headline">
+          <div className="brand" />
+          <div className="subtitle">
+            Choose Your View, Choose Your Restaurant
+          </div>
+        </div>
+        {['https://emily-winfield-eats.vercel.app',
+        'https://chandler-winfield-eats-chandlercoleman.vercel.app',
+        'https://winfield-eatss-jcrabs001.vercel.app',
+        'https://aaroneats.vercel.app',
+        'https://cobeig-winfield-eats-cobeig.vercel.app',
+        'https://katie-winfieldeats-kklimkowski.vercel.app',
+        'https://samuel-winfieldeats.vercel.app',
+        'https://ashley-winfieldeats.vercel.app',
+        'https://kaleb-winfield-eats.vercel.app',
+        'https://dae-winfield-eats.vercel.app',
+        'https://winfield-eats-ten.vercel.app'].map((link, index) => {
+          return (
+            <a href={link} style={{backgroundImage: `url("images/${index}.png")`}} />
+          ); 
+        })}
+        <div className="credit">
+          Sites and reviews by students of Basic Information and Website Design at SCKANS
+        </div>
     </div>
   );
 }
